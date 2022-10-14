5ca9a78ec29cf6f31892bcad72f983349b20b65f
Jobelle Borja <jobelleborja@DESKTOP-TIDNMRV.localdomain>
2022-10-13 22:41:54 -0500

    Gender model created.

---
+++ db/migrate/20221014034123_create_genders.rb
@@ -0,0 +1,10 @@
+class CreateGenders < ActiveRecord::Migration[7.0]
+  def change
+    create_table :genders do |t|
+      t.string :title
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
