d8dbb7ebd01966d895bcc23f8517b6d84cb9f4d7
jobelleborja <jobelleborja3@gmail.com>
2022-10-21 03:17:27 -0500

    Add simple search to application html.

--- app/controllers/villagers_controller.rb
+++ app/controllers/villagers_controller.rb
@@ -1,5 +1,6 @@
 class VillagersController < ApplicationController
   def index
+    @Villagers = Villager.search(params[:search])
     @villagers = Villager.all
   end
 end
--- app/models/villager.rb
+++ app/models/villager.rb
@@ -3,4 +3,18 @@ class Villager < ApplicationRecord
   validates :id, presence: true
   validates :title, presence: true
   validates :body, presence: true
+
+  def self.search(search)
+    if search
+      villager_name = Villager.find_by(name: villager)
+        if search
+          self.where(id: villager)
+        else
+          @Villagers = Villager.all
+         end
+        else
+          @Villagers = Villager.all
+        end
+      end
+    end
 end
