5fefb0d0c5e435ea3a7b0e5e4e5f7c52e634d3ae
jobelleborja <jobelleborja3@gmail.com>
2022-10-21 11:39:01 -0500

    About me page update, add desc

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "villagers#index"
 
   get "/villagers", to: "villagers#index"
+  get "/about", to: "about#index"
   resources :villagers, only: [:index, :show] do
     collection do
       get "search"
